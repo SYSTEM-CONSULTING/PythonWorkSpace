02f9edaf71931da46c466271233e1c00fb0efe43
david <mailto@bernhard-david.de>
2026-08-26 14:24:29 +0200

    aufraumen geht weiter

--- PlayGround/Examples/E1T0UK~E/main.py
+++
@@ -1,8 +0,0 @@
-from core.app import App
-
-def main():
-    app = App()
-    app.run()
-
-if __name__=='__main__':
-    main()
--- PlayGround/Examples/E1T0UK~E/services/printer_service.py
+++
@@ -1,3 +0,0 @@
-class PrinterService:
-    def list_printers(self): return ['HP LaserJet 4020','Brother HL-2250DN']
-    def print_document(self,doc): print(f'Drucke: {doc}')
